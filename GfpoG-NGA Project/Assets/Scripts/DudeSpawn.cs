06048dc99ddc2234c0a25aaad9d0ed681e41d7a7
Thomas Knippenberg <thomasknip27@gmail.com>
2019-01-21 19:35:53 +0100

    Prototype Game
    let's get this bread

---
+++ GfpoG-NGA Project/Assets/Scripts/DudeSpawn.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DudeSpawn : MonoBehaviour
+{
+    public GameObject dude;
+    public DudeController currentDude;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        NextDude();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void NextDude()
+    {
+        currentDude = Instantiate(dude, transform.position, Quaternion.identity).GetComponent<DudeController>(); //Spawn a new dude
+        currentDude.spawn = this;
+    }
+}
