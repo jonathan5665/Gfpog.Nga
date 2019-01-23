11119d22542fe2b58b79ceb7e54ab81a617b9918
Mo0dy <felix.muehlenberend@gmail.com>
2019-01-23 21:06:12 +0100

    Some more work on the corpses

--- GfpoG-NGA Project/Assets/Scripts/CorpseInterface.cs
+++ GfpoG-NGA Project/Assets/Scripts/CorpseInterface.cs
@@ -1,15 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 // This handles all collisions for any corpse on the scene
 public class CorpseInterface : MonoBehaviour
 {
+    // Variables
+    [Header("Fat Corpse")]
+    [Range(0, 500)]  [SerializeField] private float m_FatCorpseForce = 10f;      // the jump force the fat corpse applies
+
+    private Tilemap tilemap;
+    private LevelController level;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        tilemap = GetComponent<Tilemap>();
+        level = GetComponentInParent<LevelController>();
     }
 
     // Update is called once per frame
@@ -20,10 +28,36 @@ public class CorpseInterface : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        // check type of corpse
+        // get the player
+        CharacterController2D player = collision.gameObject.GetComponent<CharacterController2D>();
+        if (player == null)
+        {
+            return;  // only do something if colliding with a player
+        }
 
-        // pipe to correct function
-        BasicCorpse(collision);
+        // the normal on the player in the direction of the collision
+        Vector3 surfaceNormal = collision.GetContact(0).normal;
+        // add the surface Normal to the player position to get the position of the tile. This will only give the tile directly besides / under / over the player.
+        Vector3Int tilePos = tilemap.WorldToCell(collision.collider.transform.position + surfaceNormal);
+
+        TileBase tile = tilemap.GetTile(tilePos);
+
+        // if the tile that collides with the player does only collide on a corner the tile will be null
+        if (tile == null)
+        {
+            Debug.Log("ERROR no collision found");
+            return;
+        }
+
+        // check type of corpse and pipe to correct function
+        // this should be done via some kind of datastructure
+        if (tile.name == "DudeCorpse")
+        {
+            BasicCorpse(collision);
+        } else if (tile.name == "FatCorpse")
+        {
+            FatCorpse(collision);
+        }
     }
 
     // the collision function for the basic corpse
@@ -31,4 +65,11 @@ public class CorpseInterface : MonoBehaviour
     {
     }
 
+    private void FatCorpse(Collision2D collision)
+    {
+        Debug.Log("here");
+        Rigidbody2D rb = collision.gameObject.GetComponent<Rigidbody2D>();
+        Debug.Log(collision.gameObject.name);
+        rb.AddForce(new Vector2(0f, 300f));
+    }
 }   
