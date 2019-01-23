239fdbe8e89f2ac108a416024f14f6a09456d4d0
Mo0dy <felix.muehlenberend@gmail.com>
2019-01-23 14:51:46 +0100

    Added Spike Script and Camera Movement
    The Camera Movement can now be focused on a Game Object.
    
    Simple script that identifies the collision with a certain tilemap.
    Note: replace thing with GetComponent. You know what.

---
+++ GfpoG-NGA Project/Assets/Scripts/Spikes.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Spikes : MonoBehaviour
+{
+    [SerializeField] private Tilemap tilemap;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        var tilePos = tilemap.WorldToCell(collision.transform.position);
+        Debug.Log(tilePos);
+    }
+}
