57b64a7d432d0e389b2aae86ecb95949a874843e
Mo0dy <felix.muehlenberend@gmail.com>
2019-01-24 18:22:48 +0100

    Added SpawnZone for character changing

--- GfpoG-NGA Project/Assets/Scripts/LevelManager.cs
+++ GfpoG-NGA Project/Assets/Scripts/LevelManager.cs
@@ -12,6 +12,7 @@ public class LevelManager : MonoBehaviour
     [SerializeField] private int m_TotalLives;                              // the amount of deaths before a reset                          
     [SerializeField] private GameObject m_SpawnPoint;                       // a GameObject positioned where the player will spawn
     [SerializeField] private Tilemap m_CorpseMap;                           // the tilemap in which corpses will be places
+    [SerializeField] private GameObject m_SpawnZone;                        // The zone in which a character can be changed
 
     [HideInInspector] public Dropdown m_DudeSelectDropdown;                 // the dropdown menu that selects the next spawn
     [HideInInspector] public CameraMovement m_Cam;                          // the camera that will be attatched to the player
@@ -23,6 +24,8 @@ public class LevelManager : MonoBehaviour
     private CharacterController2D m_Player;                                 // the player
     private int m_CurrentLives;                                             // the amount of deaths still left
     private Tilemap m_TestMap;
+    private bool m_CanChangeCharacter = true;                                      // true if the player is allowed to change his character
+    private bool m_HasFired = false;    // used to keep the dropdown from firing twice
 
     // Start is called before the first frame update
     void Start()
@@ -42,12 +45,22 @@ public class LevelManager : MonoBehaviour
         {
             SelectDude(m_DudeSelectDropdown);
         });
+
+        // give spawnzone the event method
+        if (m_SpawnZone == null)
+        {
+            m_CanChangeCharacter = false;
+        } else
+        {
+            m_SpawnZone.GetComponent<SpawnZone>().m_OnSpawnLeave = delegate { OnSpawnLeave(); };
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // allow the dropdown to fire again
+        m_HasFired = false;
     }
 
     private void SpawnPlayer()
@@ -57,6 +70,15 @@ public class LevelManager : MonoBehaviour
 
         // attatch the player to the Camera
         m_Cam.player = m_Player.gameObject;
+
+        Debug.Log("spawned player");
+    }
+
+    private void Respawn()
+    {
+        Destroy(m_Player.gameObject);
+        SpawnPlayer();
+        m_CanChangeCharacter = true;
     }
 
     // prepares everything needed in the scene
@@ -84,12 +106,7 @@ public class LevelManager : MonoBehaviour
             m_CurrentLives -= 1;
 
             //Places the player at the spawnPoint. This very likely needs to redone once we get more characters.
-            // player.transform.position = spawnPoint.transform.position;
-
-            // switch character
-            // create a new Player instance and place it at the spawn point
-            Destroy(m_Player.gameObject);
-            SpawnPlayer();
+            Respawn();
         }
         else
         {
@@ -107,14 +124,32 @@ public class LevelManager : MonoBehaviour
     // A new dude has been selected in the dropdown menu
     public void SelectDude(Dropdown change)
     {
+        // for some reason this can be fired twice. So here I check that it was the first time;
+        if (m_HasFired)
+        {
+            return;
+        }
+        m_HasFired = true;
+
+        Debug.Log("select dude");
         string option = change.captionText.text;
+        GameObject choice = null;
 
         foreach (GameObject character in m_CharacterPrefabs)
         {
             if (character.name == change.captionText.text)
             {
-                m_NextCharacter = character;
+                choice = character;
                 break;
+            }
+        }
+
+        if (choice != null)
+        {
+            m_NextCharacter = choice;
+            if (m_CanChangeCharacter)
+            {
+                Respawn();
             }
         }
     }
@@ -127,5 +162,12 @@ public class LevelManager : MonoBehaviour
     public int GetCurrLives()
     {
         return m_CurrentLives;
+    }
+
+    // called when the character leaves the spawn zone
+    public void OnSpawnLeave()
+    {
+        m_CanChangeCharacter = false;
+        Debug.Log("left spawn zone");
     }
 }
---
+++ GfpoG-NGA Project/Assets/Scripts/SpawnZone.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnZone : MonoBehaviour
+{
+    [HideInInspector] public delegate void OnSpawnLeave();
+    [HideInInspector] public OnSpawnLeave m_OnSpawnLeave;
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        // maybe we should check if the player leaves the spawn
+        m_OnSpawnLeave();
+    }
+}
